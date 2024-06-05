2f8e8170b1ee6742de1158a1a9b70ca2f12262c5
Ibullayev Zhanibek <ibullayevzhanibek@gmail.com>
2024-06-06 02:41:40 +0500

    Initial Commit

---
+++ StackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Zhanibek Ibullayev on 06.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
